37b80415c840d2651b4cf75e628c95bd038666ef
pseudo-rnd-thoughts <mark.m.towers@gmail.com>
2022-09-13 10:22:09 +0100

    Update to gymnasium

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="gym-notices",
-    version="0.0.8",
+    name="gymnasium-notices",
+    version="0.0.1",
     author="Jordan Terry",
     author_email="jkterry0@farama.org",
-    description="Notices for gym",
+    description="Notices for gymnasium",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Farama-Foundation/gym-notices",
